00362ae671c66a821dd1388a4130ca50d416cfa9
Thiago Santana <thiagodssantana@gmail.com>
2022-05-10 19:29:52 -0300

    Aula de operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,338 @@
+//#1 SOMA
+
+// const valor1 = 20
+// const valor2 = Number(prompt("Informe um número qualquer"))
+
+// // const resultado = valor1 + valor2
+
+// console.log(valor1 + valor2)
+
+//#SUBTRAÇÃO
+
+// const primeiroValor = 10
+// const segundoValor = 5
+
+// const resultado = primeiroValor - segundoValor
+
+// console.log(resultado)
+
+//#MULTIPLICAÇÃO
+
+// const primeiroValor = 50
+// const valorMultiplicador = 5
+
+// const resultado = primeiroValor * valorMultiplicador
+
+// console.log(resultado)
+
+//#DIVISAO
+
+// const primeiroValor = 50
+// const valorDivisor = 5
+
+// const resultado = primeiroValor / valorDivisor
+
+// console.log(resultado)
+
+//#SOMA E MULTIPLICA
+
+// const valor1 = 10
+// const valor2 = 15
+// const valor3 = 3
+
+// const resultado = valor1 + valor2 * valor3
+
+// console.log(resultado)
+
+//#DIVISAO E MULTIPLICACAO
+
+// const valor1 = 10
+// const valor2 = 15
+// const valor3 = 3
+
+// const resultado = valor1 / (valor2 * valor3)
+
+// console.log(resultado)
+
+//#MODULO
+
+//PAR OU IMPAR
+
+// const valor1 = 11
+// const valor2 = 2
+
+// const resultado = valor1 % valor2
+
+// console.log(resultado)
+
+// const valor1 = 25
+// const valor2 = 2
+
+// const resultado = valor1 % valor2
+
+// console.log(resultado)
+
+// let minhaIdade = 27
+
+// minhaIdade = minhaIdade + 1
+// minhaIdade += 1
+
+// console.log(minhaIdade)
+
+//#SIMPLIFICAÇÃO
+
+// let valor1 = 20
+// let valor2 = 30
+// let valor3 = 50
+
+
+// // let resultado1 = valor1 *= 2
+
+// let resultado1 = valor1 * 2
+// let resultado2 = valor2 /= 2
+// let resultado3 = valor3 -= 30
+// let resultado4 = valor3 += 50
+// console.log( resultado4)
+
+//EXERCICIO1 
+
+// let primeiroValor = 3
+// let segundoValor = 4 
+
+// const resultado = primeiroValor + segundoValor
+
+// console.log(resultado)
+
+//#exercicio2
+
+// let valor1 = 3
+// let valor2 = 5
+// let valor3 = 2
+
+// const resultado = (valor1 * valor2) / valor3
+// console.log((3 * 5) / 2)
+
+//#exercicio3 
+// let valor1 = 4
+// let valor2 = 5
+// let valorMultiplicador = -1
+
+// const resultado = (valor1 - valor2) * valorMultiplicador 
+// console.log(resultado)
+
+//#RESTO DA DIVISAO
+
+// let valor1 = 234
+
+// let valor2 = 5
+
+// const resultado = valor1 % valor2
+
+// console.log(resultado)
+
+//---------------------- COMPARADORES -----------------
+//#IGUALDADE
+// const condicao = 1 === 2
+
+// const condicao2 = "1" === "1"
+
+
+// console.log(condicao2)
+
+//#DIFERENTE
+
+// const condicao = 1 !== 2
+
+// console.log(condicao)
+
+//#>=
+
+// const condicao = 1 > 1
+// const condicao = 1 >= 1
+// const idade = 18
+// const maiorDeIdade = idade >= 18
+
+// const ateCinquentaAnos = idade <= 50
+
+// console.log(ateCinquentaAnos)
+
+//#EXERCICIOS COMPARADORES
+
+//#1 
+
+// const primeiroValor = "5"
+// const segundoValor = 5
+
+// const resultado = primeiroValor == segundoValor
+
+// console.log(resultado)
+
+// const primeiroValor = 5
+// const segundoValor = 5
+
+// const resultado = primeiroValor === segundoValor
+
+// console.log(resultado)
+
+//#2 DIFERENTE
+
+// const valor1 = 4
+// const valor2 = 4
+
+// const resultado = valor1 !== valor2
+
+// console.log(resultado)
+
+//#maior que
+
+// const valor1 = 10
+// const valor2 = 15
+
+// // const resultado = valor2 > valor1
+// const resultado = valor1 > valor2
+
+// console.log(resultado)
+
+//#menor que
+
+// const valor1 = 50
+// const valor2 = 20
+
+// // const resultado = valor2 < valor1
+// const resultado = valor1 < valor2
+// console.log(resultado)
+
+// const idade = 55
+
+// const maiorDeIdade = idade >= 18
+// const ateCinquentaAnos = idade <= 50
+//                 //true       //false
+// console.log(maiorDeIdade && ateCinquentaAnos)
+
+//#EXERCICIO &&
+
+// const a = true
+// const b = false
+// const c = true
+
+// console.log("a && b:", a && b)
+
+// console.log("b && c:", b && c)
+
+// console.log("a && c:", a && c)
+
+// console.log("a && b && c:", a && b && c)
+
+// const idade = 31
+
+// const maiorQue18 = idade > 31
+// const menorQue30 = idade < 30
+
+// console.log(maiorQue18, menorQue30)
+// console.log("resultado:", maiorQue18 || menorQue30)
+
+//#exercicio ||
+
+// const a = true
+// const b = false
+// const c = true
+
+// // console.log("a || b:", a || b)
+
+// // console.log("b || c:", b || c)
+
+// // console.log("a || c", a || c)
+
+// // console.log("a || b || c:", a || b || c)
+
+// // console.log(b || b)
+
+// console.log(!a)
+
+//#exercicio
+
+// const nome = "Julia"
+// const idade = 24
+// const eMaiorDeIdade = idade >= 18
+// const idade2050 = (2050 -2022) + idade
+
+// console.log(`A ${nome} tem ${idade} anos, é maior de idade (${eMaiorDeIdade}) e em 2050 ela terá ${idade2050} anos em 2050`)
+
+
+//# Exercício 1
+
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
+
+// let resultado = bool1 && bool2
+// console.log("a. false pois temos um true e um false", resultado)
+
+// resultado = bool1 && bool2 && bool3 
+// console.log("b. false pois temos um false", resultado) 
+
+// resultado = !resultado && (bool1 || bool2) 
+// console.log("c. true pois o oposto do resultado anterior é true e o resultado dos parenteses tambem é true", resultado)
+
+// console.log("d. string pos o resultado resorna texto", typeof resultado)
+//                "Boolean, pois o type of retorna o tipo)
+
+
+//# Exercício 2
+
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// let segundoNumero = Number(prompt("Digite outro numero!"))
+// // Se não estiver como number, ele retornará texto e vai imprimir um número seguido do outro.
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+
+//# Exercício 1 de escrita de código
+
+// Para imprimir a soma, ele deve codar da seguinte maneira:
+
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// let segundoNumero = Number(prompt("Digite outro numero!"))
+// // Se não estiver como number, ele retornará texto e vai imprimir um número seguido do outro.
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+// const idade = Number(prompt("Qual a sua idade?"));
+// const idadeDoAmigo = Number(prompt("Qual a idade da sua melhor amiga ou melhor amigo?"));
+
+// console.log ("Sua idade é maior do que a do seu melhor amigo?", idade>idadeDoAmigo)
+
+// console.log (idade - idadeDoAmigo)
+
+// #Exercício 2 de escrita de código
+
+// const numeroPar = Number(prompt("insira um número par"));
+// console.log(numeroPar%2);
+// Ele sempre retorna zero, pois divisão de números pares não tem sobras.
+// Se ele colocar um número ímpar, o resultado é a sobra da conta.
+
+// #Exercício 3
+
+// const idade = Number(prompt("Quantos anos você tem?"));
+
+
+// console.log("Você tem", idade*12, "Meses,", idade*365,"dias e", idade*365*24, "horas de idade");
+
+// @Exercício 4
+
+const numero1 = Number(prompt("Insira um número"))
+const numero2 = Number(prompt("Insira outro número"))
+
+const resultado1 = numero1>numero2
+const resultado2 = numero1===numero2
+const resultado3 = (numero1%numero2)===0
+const resultado4 = (numero2%numero1)===0
+
+console.log("O primeiro número é maior que o segundo?", resultado1);
+console.log("O primeiro número é igual ao segundo?", resultado2);
+console.log("O primeiro número é divisível pelo segundo?", resultado3);
+console.log("O segundo número é divisível pelo primeiro?", resultado4);
+
+
