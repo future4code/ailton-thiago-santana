770328f125c50d2d7431a632ee6509cc033d89f5
Thiago Santana <thiagodssantana@gmail.com>
2022-05-09 22:13:21 -0300

    variaveis

---
+++ modulo1/variavel/template_javascript/index.js
@@ -0,0 +1,97 @@
+/*const primeiroNome = "Thiago";
+const sobrenome = "Santana";
+const idade = 30
+const statusDeEstudante = true;
+
+console.log ("Meu nome é", primeiroNome, sobrenome, "eu tenho", idade, "anos e meu status de estudante é", statusDeEstudante);
+*/
+
+/*const got = "Game of Thrones"
+const temporadasDeGot = 8
+
+typeof got //straing
+typeof temporadasDeGot //nunber
+
+console.log (typeof got)
+console.log (typeof temporadasDeGot)
+*/
+
+/*let minhaVariavel;
+console.log(minhaVariavel);
+
+minhaVariavel = null;
+console.log(minhaVariavel);
+ 
+const nome = prompt ("qual é o seu nome?");
+const idade = prompt ("qual é a sua idade")
+
+console.log(typeof nome, typeof idade); 
+
+
+
+const idade = 19;
+onst idadeTexto = idade.toString();
+
+
+console.log(typeof idadeTexto);
+
+let a = 10
+let b = 10
+
+console.log(b)
+b = 5
+console.log(a, b)
+*/
+
+//(a = 10, b = 10)
+
+/*
+let a = 10
+let b = 20
+c = a
+b = c
+a = b 
+
+console.log(a, b, c)*/
+
+//p = horasDeTrabalho
+//t = salarioDia
+
+
+//const nome = null;
+//const idade = null;
+
+//console.log(typeof nome, typeof idade);
+
+// imprimiu object porque null representa objeto
+
+/*const nome = prompt ("Qual o seu nome?");
+const idade = prompt ("Qual a sua idade?");
+
+console.log (typeof nome, typeof idade);
+//Dessa vez ele retornou string, pois as respostas atribuiram textos às variáveis
+
+console.log ("Olá", nome, "você tem", idade, "anos");
+
+
+
+let p1 = prompt ("Você gosta de música?");
+let p2 = prompt ("Você sabe jogar gamão?");
+let p3 = prompt ("Você sabe soltar pipa?");
+
+console.log ("Você gosta de música?",p1);
+console.log ("Você sabe jogar gamão?",p2);
+console.log ("Você sabe soltar pipa?",p3);
+
+
+let a = 10
+let b = 25
+c = a
+a = b
+b = c
+
+console.log ("O novo valor de a é", a);
+console.log ("O novo valor de b é", b);*/
+
+
+
