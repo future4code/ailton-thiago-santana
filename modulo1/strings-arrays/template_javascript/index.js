b2e9d64fe02e0888cb6921b760bba1df555654a7
Thiago Santana <thiagodssantana@gmail.com>
2022-05-12 22:11:58 -0300

    strings-arrays

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,84 @@
+// 1 - Crie um programa que peça ao usuário para inserir o seu
+// nome e sua cor favorita e imprima a mensagem:
+// "A cor favorita de FULANO é COR"
+
+// 2 -Faça o exercício duas vezes, utilizando template strings e
+// concatenação
+
+
+// const nome = prompt("Qual o seu nome?");
+// const corFavorita = prompt("Qual a sua cor favorita?");
+
+// console.log(`A cor favorita de ${nome} é ${corFavorita}`);
+
+// console.log("A cor favorita de " +nome+ " é " +corFavorita);
+
+// const nome = "Caio Teixeira";
+// console.log(nome.length)
+
+
+// 1 - Peça para o usuário escrever uma frase e imprima no console
+// a frase alterada, com:
+
+// 1.1 - Todas as letras maiúsculas
+// 1.2 - Na língua do i (substituindo todas as vogais por i)
+// 1.3 - Retorne também o tamanho da frase.
+
+// const frase = prompt("Quais os seus 3 filmes favoritos?");
+// console.log(frase.replaceAll("o","i").replaceAll("a","i").replaceAll("e","i").replaceAll("u","i"));
+// console.log(frase.toUpperCase());
+// console.log(frase.length);
+
+// let array
+// console.log('a. ', array) //undefined
+
+// array = null
+// console.log('b. ', array)//null
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length) //11 
+
+// let i = 0
+// console.log('d. ', array[i]) //3
+
+// array[i+1] = 19
+// console.log('e. ', array) //[3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+// const valor = array[i+6]
+// console.log('f. ', valor)//9
+
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+//"SUBI NUM ÔNIBUS EM MIRROCOS" 27
+
+// const nome = prompt("Digite seu nome");
+// const email = prompt("Digite seu e mail");
+
+// console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome}!`)
+
+// let comidaFavorita = ["Lasanha", "Pizza", "Churrasco", "Acarajé", "Podrão"];
+// let usuario = prompt("Insira sua comida favorita")
+// comidaFavorita[1] = usuario
+// console.log(`Essas são as minhas comidas preferidas: 
+// ${comidaFavorita[0]}
+// ${comidaFavorita[1]}
+// ${comidaFavorita[2]}
+// ${comidaFavorita[3]}
+// ${comidaFavorita[4]}`)
+
+
+// let listaDeTarefas = []
+// let tarefa1 = prompt("Digite a primeira tarefa")
+// let tarefa2 = prompt("Digite a segunda tarefa")
+// let tarefa3 = prompt("Digite a terceira tarefa")
+// listaDeTarefas = [tarefa1, tarefa2, tarefa3]
+// //console.log(listaDeTarefas)
+// let tarefaRealizada = prompt("Qual tarefa já foi realizada, considerando a primeira = 0, a segunda=1 e a terceira=2")
+// let listaAtualizada = listaDeTarefas.splice(tarefaRealizada,1)
+// console.log(listaDeTarefas)
+
+
+
+
