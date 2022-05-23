bbc663508fc92a1ba43f1b33df0ec5be9fff5b43
Thiago Santana <thiagodssantana@gmail.com>
2022-05-22 23:08:40 -0300

    Exercício 3

--- modulo1/revisao-postinho/index.js
+++ modulo1/revisao-postinho/index.js
@@ -30,22 +30,40 @@
 // // exemplo 1>2 = false
 // // a-) 1==='1'= false, pois um é number e o outro é string
 // // b-) 1=='1'= false, pois um é number e o outro é string
-// // c-) 'a'==='b'= false
-// // d-) 'b'>'a'= true
-// // e-) 0!==null= true
+// // c-) 'a'==='b'= false, pois 'a'!=='b'
+// // d-) 'b'>'a'= true, pois 'b' é maior que 'a' de acordo com a posição no alfabeto
+// // e-) 0!==null= true, pois indicam coisas
 
 
 // // CONDICIONAIS
 
 // // Exercício 3------------------------------------------------------------------------------------
 
-// const cadastro = () => {
-//     const usuario = []
+const cadastro = () => {
+    const usuario = []
+    //  Sua lógica aqui
+const nomeDeUsuario = prompt('Digite seu nome')
+const anoDeNascimento = prompt('Digite seu ano de nascimento')
+const senha = prompt('Crie uma senha')
+const nacionalidade = prompt('Insira sua nacionalidade')
 
-//     //  Sua lógica aqui
+if (anoDeNascimento <= 2004) {
+    usuario.push(anoDeNascimento)
+} else {
+    console.log('Para se cadastrar, você precisa ser maior de idade')
+} if (nacionalidade === 'brasileira') {
+    usuario.push(nacionalidade)
+} else {
+    console.log('Para se cadastrar, precisa ter nacionalidade brasileira')
+} if (senha.length >= 6){
+    usuario.push(senha)
+} else {
+    console.log('senha precisa ter 6 ou mais caracteres')
+}
+console.log(usuario)
+}
+cadastro()
 
-// }
-// console.log(cadastro());
 
 // // Exercício 4-----------------------------------------------------------------------------------------------
 
